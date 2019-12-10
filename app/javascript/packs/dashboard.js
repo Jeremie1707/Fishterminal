2355ca53c097da90b4afa13cb29b32132efcb305
Jeremie Poiraudau <jeremie.poiraudau@lafokale.com>
2019-12-10 05:13:28 +0100

    faster db with datatables

--- app/javascript/packs/dashboard.js
+++ app/javascript/packs/dashboard.js
@@ -1,8 +1,8 @@
- $(document).ready(function() {
-  $("#loadins").dataTable();
-});
+//  $(document).ready(function() {
+//   $("#loadins").dataTable();
+// });
 
- $(document).ready(function() {
-  $("#loadouts").dataTable();
-});
+//  $(document).ready(function() {
+//   $("#loadouts").dataTable();
+// });
 
